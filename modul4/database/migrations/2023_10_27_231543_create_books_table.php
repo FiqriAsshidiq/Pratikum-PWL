82493bc8cf01cbc62a50b43f95886b737b618e38
fiqriasshidiq20@gmail.com <fiqriasshidiq20@gmail.com>
2023-11-29 11:56:10 +0700

    perubahab

---
+++ modul4/database/migrations/2023_10_27_231543_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id('book_id');
+            $table->string ('title', 255);
+            $table->string ('author', 255);
+            $table->year ('year');
+            $table->string ('publisher', 255);
+            $table->string ('city', 255);
+            $table->string ('cover', 255);
+            $table->unsignedBigInteger('bookshelf_id');
+            $table->timestamps();
+
+            $table->foreign('bookshelf_id')->references('bookshelf_id')->on('bookshelfs')
+            ->onDelete('cascade')      
+            ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
